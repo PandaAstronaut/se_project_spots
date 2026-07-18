f3c7128cb7884a965f8a77a63c8ee19cea0fae3c
Seth Milleson <seth.milleson@gmail.com>
2026-07-18 10:40:25 -0600

    spots stage 5 v2

--- scripts/index.js
+++ scripts/index.js
@@ -2,37 +2,9 @@ const profileEditBtn = document.querySelector(".profile__edit-btn");
 const profileEditModal = document.querySelector("#edit-profile-modal");
 const profileEditCloseBtn = profileEditModal.querySelector(".modal__close-btn");
 
-function profileEditBtnClick() {
-  profileEditModal.classList.add("modal_is-opened");
-}
-
-function profileEditCloseBtnClick() {
-  profileEditModal.classList.remove("modal_is-opened");
-}
-
-profileEditBtn.addEventListener("click", profileEditBtnClick);
-
-profileEditCloseBtn.addEventListener("click", profileEditCloseBtnClick);
-
-const newPostBtn = document.querySelector(".profile__add-btn");
-const newPostModal = document.querySelector("#new-post-modal");
-const newPostCloseBtn = newPostModal.querySelector(".modal__close-btn");
-
-function newPostBtnClick() {
-  newPostModal.classList.add("modal_is-opened");
-}
-
-function newPostCloseBtnClick() {
-  newPostModal.classList.remove("modal_is-opened");
-}
-
-newPostBtn.addEventListener("click", newPostBtnClick);
-
-newPostCloseBtn.addEventListener("click", newPostCloseBtnClick);
-
 const profileNameText = document.querySelector(".profile__name");
 const profileDescriptionText = document.querySelector(".profile__description");
-const editProfileForm = document.querySelector(".modal__form");
+const editProfileForm = profileEditModal.querySelector(".modal__form");
 const profileNameInputText = editProfileForm.querySelector(
   "#profile-name-input",
 );
@@ -40,27 +12,61 @@ const profileDescriptionInputText = editProfileForm.querySelector(
   "#profile-description-input",
 );
 
-function handleProfileFormSubmit(evt) {
-  evt.preventDefault();
-  profileNameText.textContent = profileNameInputText.value;
-  profileDescriptionText.textContent = profileDescriptionInputText.value;
-
-  profileEditModal.classList.remove("modal_is-opened");
-}
-
-editProfileForm.addEventListener("submit", handleProfileFormSubmit);
+const newPostBtn = document.querySelector(".profile__add-btn");
+const newPostModal = document.querySelector("#new-post-modal");
+const newPostCloseBtn = newPostModal.querySelector(".modal__close-btn");
 
 const addBtnFormElement = newPostModal.querySelector(".modal__form");
 const cardLinkInput = newPostModal.querySelector("#card-image-input");
 const cardNameInput = newPostModal.querySelector("#card-caption-input");
 
-function handleAddCardSubmit(evt) {
-  evt.preventDefault();
-
-  console.log(cardLinkInput.value);
-  console.log(cardNameInput.value);
-
-  newPostModal.classList.remove("modal_is-opened");
+function openModal(modal) {
+  modal.classList.add("modal_is-opened");
 }
 
+function closeModal(modal) {
+  modal.classList.remove("modal_is-opened");
+}
+
+function openProfileEditModal() {
+  profileNameInputText.value = profileNameText.textContent;
+
+  profileDescriptionInputText.value = profileDescriptionText.textContent;
+
+  openModal(profileEditModal);
+}
+
+function closeProfileEditModal() {
+  closeModal(profileEditModal);
+}
+
+function openNewPostModal() {
+  openModal(newPostModal);
+}
+
+function closeNewPostModal() {
+  closeModal(newPostModal);
+}
+
+function handleProfileFormSubmit(evt) {
+  evt.preventDefault();
+  profileNameText.textContent = profileNameInputText.value;
+  profileDescriptionText.textContent = profileDescriptionInputText.value;
+  closeModal(profileEditModal);
+}
+
+function handleAddCardSubmit(evt) {
+  evt.preventDefault();
+  console.log(cardLinkInput.value);
+  console.log(cardNameInput.value);
+  closeModal(newPostModal);
+}
+
+profileEditBtn.addEventListener("click", openProfileEditModal);
+editProfileForm.addEventListener("submit", handleProfileFormSubmit);
 addBtnFormElement.addEventListener("submit", handleAddCardSubmit);
+profileEditCloseBtn.addEventListener("click", () =>
+  closeModal(profileEditModal),
+);
+newPostBtn.addEventListener("click", () => openModal(newPostModal));
+newPostCloseBtn.addEventListener("click", () => closeModal(newPostModal));
